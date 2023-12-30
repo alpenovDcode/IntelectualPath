ad0704ac634e1234350b41e8f3bcfd7e9ef77ef0
alpenovDcode <alpenovevgeniy05@gmail.com>
2023-12-30 21:57:17 +0600

    Initial Commit

---
+++ IntelectualPath/IntelectualPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntelectualPathApp.swift
+//  IntelectualPath
+//
+//  Created by Альпеша on 30.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct IntelectualPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
